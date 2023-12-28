54a347e114d29319787f0bc62a9d0c2a5f1f69ab
beer <beer5214@126.com>
2023-12-28 20:36:41 +0800

    add garmin_secret.py run param

--- garmin_secret.py
+++ garmin_secret.py
@@ -1,4 +1,7 @@
+import argparse
+
 import garth
+import sys
 
 
 class GarminLogin:
@@ -21,3 +24,17 @@ class GarminLogin:
         except Exception as e:
             print(e)
             return ""
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--user", action="store", required=True, help="email")
+    parser.add_argument("-p", "--pwd", action="store", required=True, help="pwd")
+    parser.add_argument("--cn", action="store_true", help="if garmin accout is cn",
+                        )
+    options = parser.parse_args()
+    print(options)
+    login = GarminLogin(options.user, options.pwd, options.cn)
+    r = login.gen_secret()
+    if r != "":
+        print('success')
