490366708fc873b34d55c0c8af415865093fce3b
beer <beer5214@126.com>
2024-04-15 12:02:55 +0800

    add garmin_upload.py

---
+++ upload/garmin_upload.py
@@ -0,0 +1,37 @@
+import argparse
+import sys
+import garth
+
+
+def main(user: str, pwd: str, is_cn: bool, gpx_file: str):
+    if not user:
+        exit(1)
+
+    if is_cn:
+        garth.configure(domain="garmin.cn")
+
+    try:
+        garth.login(user, pwd)
+    except:
+        print('login failed')
+        exit(1)
+
+    with open(gpx_file, "rb") as f:
+        uploaded = garth.client.upload(f)
+        print(uploaded)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--user", action="store", required=True, help="email")
+    parser.add_argument("-p", "--pwd", action="store", required=True, help="pwd")
+    parser.add_argument("-f", "--file", action="store", required=True, help="file")
+    parser.add_argument(
+        "--is-cn",
+        dest="is_cn",
+        action="store_true",
+        help="if garmin accout is cn",
+    )
+
+    options = parser.parse_args()
+    main(options.user, options.pwd, options.is_cn, options.file)
