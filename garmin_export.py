a3c3d1501ffba9ee17511d9b6754a81cfa447c3e
beer <beer5214@126.com>
2024-06-24 20:59:29 +0800

    support fit download

--- garmin_export.py
+++ garmin_export.py
@@ -5,114 +5,221 @@ Copy most code from https://github.com/cyberjunky/python-garminconnect
 
 import argparse
 import asyncio
-import logging
+import datetime
 import os
-import sys
 import time
 import traceback
 import zipfile
 
-import aiofiles
-import cloudscraper
 import garth
-import httpx
 
 from garmin_secret import GarminLogin
 
-logger = logging.getLogger(__name__)
-
-TIME_OUT = httpx.Timeout(240.0, connect=360.0)
-GARMIN_COM_URL_DICT = {
-    "SSO_URL_ORIGIN": "https://sso.garmin.com",
-    "SSO_URL": "https://sso.garmin.com/sso",
-    "MODERN_URL": "https://connectapi.garmin.com",
-    "SIGNIN_URL": "https://sso.garmin.com/sso/signin",
-    "UPLOAD_URL": "https://connectapi.garmin.com/upload-service/upload/",
-    "ACTIVITY_URL": "https://connectapi.garmin.com/activity-service/activity/{activity_id}",
-}
-
-GARMIN_CN_URL_DICT = {
-    "SSO_URL_ORIGIN": "https://sso.garmin.com",
-    "SSO_URL": "https://sso.garmin.cn/sso",
-    "MODERN_URL": "https://connectapi.garmin.cn",
-    "SIGNIN_URL": "https://sso.garmin.cn/sso/signin",
-    "UPLOAD_URL": "https://connectapi.garmin.cn/upload-service/upload/",
-    "ACTIVITY_URL": "https://connectapi.garmin.cn/activity-service/activity/{activity_id}",
-}
-
 
 class Garmin:
-    def __init__(self, secret_string, auth_domain, is_only_running=False):
-        """
-        Init module
-        """
-        self.req = httpx.AsyncClient(timeout=TIME_OUT)
-        self.cf_req = cloudscraper.CloudScraper()
-        self.URL_DICT = (
-            GARMIN_CN_URL_DICT
-            if auth_domain and str(auth_domain).upper() == "CN"
-            else GARMIN_COM_URL_DICT
+    def __init__(self, secret_path, is_cn):
+        """Create a new class instance."""
+        self.is_cn = is_cn
+
+        self.garmin_connect_user_settings_url = (
+            "/userprofile-service/userprofile/user-settings"
         )
-        if auth_domain and str(auth_domain).upper() == "CN":
-            garth.configure(domain="garmin.cn")
-        self.modern_url = self.URL_DICT.get("MODERN_URL")
-        garth.client.loads(secret_string)
-        if garth.client.oauth2_token.expired:
-            garth.client.refresh_oauth2()
+        self.garmin_connect_devices_url = (
+            "/device-service/deviceregistration/devices"
+        )
+        self.garmin_connect_device_url = "/device-service/deviceservice"
 
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
-            "origin": self.URL_DICT.get("SSO_URL_ORIGIN"),
-            "nk": "NT",
-            "Authorization": str(garth.client.oauth2_token),
+        self.garmin_connect_primary_device_url = (
+            "/web-gateway/device-info/primary-training-device"
+        )
+
+        self.garmin_connect_solar_url = "/web-gateway/solar"
+        self.garmin_connect_weight_url = "/weight-service"
+        self.garmin_connect_daily_summary_url = (
+            "/usersummary-service/usersummary/daily"
+        )
+        self.garmin_connect_metrics_url = (
+            "/metrics-service/metrics/maxmet/daily"
+        )
+        self.garmin_connect_daily_hydration_url = (
+            "/usersummary-service/usersummary/hydration/daily"
+        )
+        self.garmin_connect_set_hydration_url = (
+            "usersummary-service/usersummary/hydration/log"
+        )
+        self.garmin_connect_daily_stats_steps_url = (
+            "/usersummary-service/stats/steps/daily"
+        )
+        self.garmin_connect_personal_record_url = (
+            "/personalrecord-service/personalrecord/prs"
+        )
+        self.garmin_connect_earned_badges_url = "/badge-service/badge/earned"
+        self.garmin_connect_adhoc_challenges_url = (
+            "/adhocchallenge-service/adHocChallenge/historical"
+        )
+        self.garmin_connect_badge_challenges_url = (
+            "/badgechallenge-service/badgeChallenge/completed"
+        )
+        self.garmin_connect_available_badge_challenges_url = (
+            "/badgechallenge-service/badgeChallenge/available"
+        )
+        self.garmin_connect_non_completed_badge_challenges_url = (
+            "/badgechallenge-service/badgeChallenge/non-completed"
+        )
+        self.garmin_connect_inprogress_virtual_challenges_url = (
+            "/badgechallenge-service/virtualChallenge/inProgress"
+        )
+        self.garmin_connect_daily_sleep_url = (
+            "/wellness-service/wellness/dailySleepData"
+        )
+        self.garmin_connect_daily_stress_url = (
+            "/wellness-service/wellness/dailyStress"
+        )
+        self.garmin_connect_hill_score_url = (
+            "/metrics-service/metrics/hillscore"
+        )
+
+        self.garmin_connect_daily_body_battery_url = (
+            "/wellness-service/wellness/bodyBattery/reports/daily"
+        )
+
+        self.garmin_connect_blood_pressure_endpoint = (
+            "/bloodpressure-service/bloodpressure/range"
+        )
+
+        self.garmin_connect_set_blood_pressure_endpoint = (
+            "/bloodpressure-service/bloodpressure"
+        )
+
+        self.garmin_connect_endurance_score_url = (
+            "/metrics-service/metrics/endurancescore"
+        )
+        self.garmin_connect_menstrual_calendar_url = (
+            "/periodichealth-service/menstrualcycle/calendar"
+        )
+
+        self.garmin_connect_menstrual_dayview_url = (
+            "/periodichealth-service/menstrualcycle/dayview"
+        )
+        self.garmin_connect_pregnancy_snapshot_url = (
+            "periodichealth-service/menstrualcycle/pregnancysnapshot"
+        )
+        self.garmin_connect_goals_url = "/goal-service/goal/goals"
+
+        self.garmin_connect_rhr_url = "/userstats-service/wellness/daily"
+
+        self.garmin_connect_hrv_url = "/hrv-service/hrv"
+
+        self.garmin_connect_training_readiness_url = (
+            "/metrics-service/metrics/trainingreadiness"
+        )
+
+        self.garmin_connect_race_predictor_url = (
+            "/metrics-service/metrics/racepredictions"
+        )
+        self.garmin_connect_training_status_url = (
+            "/metrics-service/metrics/trainingstatus/aggregated"
+        )
+        self.garmin_connect_user_summary_chart = (
+            "/wellness-service/wellness/dailySummaryChart"
+        )
+        self.garmin_connect_floors_chart_daily_url = (
+            "/wellness-service/wellness/floorsChartData/daily"
+        )
+        self.garmin_connect_heartrates_daily_url = (
+            "/wellness-service/wellness/dailyHeartRate"
+        )
+        self.garmin_connect_daily_respiration_url = (
+            "/wellness-service/wellness/daily/respiration"
+        )
+        self.garmin_connect_daily_spo2_url = (
+            "/wellness-service/wellness/daily/spo2"
+        )
+        self.garmin_all_day_stress_url = (
+            "/wellness-service/wellness/dailyStress"
+        )
+        self.garmin_connect_activities = (
+            "/activitylist-service/activities/search/activities"
+        )
+        self.garmin_connect_activity = "/activity-service/activity"
+        self.garmin_connect_activity_types = (
+            "/activity-service/activity/activityTypes"
+        )
+        self.garmin_connect_activity_fordate = (
+            "/mobile-gateway/heartRate/forDate"
+        )
+        self.garmin_connect_fitnessstats = "/fitnessstats-service/activity"
+
+        self.garmin_connect_fit_download = "/download-service/files/activity"
+        self.garmin_connect_tcx_download = (
+            "/download-service/export/tcx/activity"
+        )
+        self.garmin_connect_gpx_download = (
+            "/download-service/export/gpx/activity"
+        )
+        self.garmin_connect_kml_download = (
+            "/download-service/export/kml/activity"
+        )
+        self.garmin_connect_csv_download = (
+            "/download-service/export/csv/activity"
+        )
+
+        self.garmin_connect_upload = "/upload-service/upload"
+
+        self.garmin_connect_gear = "/gear-service/gear/filterGear"
+        self.garmin_connect_gear_baseurl = "/gear-service/gear/"
+
+        self.garmin_request_reload_url = (
+            "/wellness-service/wellness/epoch/request"
+        )
+
+        self.garmin_workouts = "/workout-service"
+
+        self.garmin_connect_delete_activity_url = "/activity-service/activity"
+
+        self.garth = garth.Client(
+            domain="garmin.cn" if is_cn else "garmin.com"
+        )
+
+        self.garth.load(secret_path)
+        if self.garth.oauth2_token.expired:
+            self.garth.refresh_oauth2()
+
+    def connectapi(self, path, **kwargs):
+        return self.garth.connectapi(path, **kwargs)
+
+    def download_activity(self, activity_id):
+        url = f"/download-service/files/activity/{activity_id}"
+        return self.download(url)
+
+    def download(self, path, **kwargs):
+        return self.garth.download(path, **kwargs)
+
+    def get_activity_id_list(self):
+        activities = []
+        start = 0
+        limit = 20
+        url = self.garmin_connect_activities
+        today = datetime.date.today()
+        start_date = today - datetime.timedelta(days=100)
+        params = {
+            "startDate": str(start_date.isoformat()),
+            "endDate": str(today.isoformat()),
+            "start": str(start),
+            "limit": str(limit),
         }
-        self.is_only_running = is_only_running
-        self.upload_url = self.URL_DICT.get("UPLOAD_URL")
-        self.activity_url = self.URL_DICT.get("ACTIVITY_URL")
-
-    async def fetch_data(self, url, retrying=False):
-        """
-        Fetch and return data
-        """
-        try:
-            response = await self.req.get(url, headers=self.headers)
-            if response.status_code == 429:
-                raise GarminConnectTooManyRequestsError("Too many requests")
-            logger.debug(f"fetch_data got response code {response.status_code}")
-            response.raise_for_status()
-            return response.json()
-        except Exception as err:
-            print(err)
-            if retrying:
-                logger.debug(
-                    "Exception occurred during data retrieval, relogin without effect: %s"
-                    % err
-                )
-                raise GarminConnectConnectionError("Error connecting") from err
+        while True:
+            params["start"] = str(start)
+            act = self.connectapi(url, params=params)
+            if act:
+                activities.extend(act)
+                start = start + limit
             else:
-                logger.debug(
-                    "Exception occurred during data retrieval - perhaps session expired - trying relogin: %s"
-                    % err
-                )
-                await self.fetch_data(url, retrying=True)
-
-    async def get_activities(self, start, limit):
-        """
-        Fetch available activities
-        """
-        url = f"{self.modern_url}/activitylist-service/activities/search/activities?start={start}&limit={limit}"
-        if self.is_only_running:
-            url = url + "&activityType=running"
-        return await self.fetch_data(url)
-
-    async def download_activity(self, activity_id, file_type="gpx"):
-        url = f"{self.modern_url}/download-service/export/{file_type}/activity/{activity_id}"
-        if file_type == "fit":
-            url = f"{self.modern_url}/download-service/files/activity/{activity_id}"
-        logger.info(f"Download activity from {url}")
-        response = await self.req.get(url, headers=self.headers)
-        response.raise_for_status()
-        return response.read()
+                break
+        return [str(item["activityId"]).split("_")[0]
+                for item in activities
+                if item['activityType']['typeKey'] != 'other'
+                ]
 
 
 class GarminConnectHttpError(Exception):
@@ -148,38 +255,28 @@ class GarminConnectAuthenticationError(Exception):
         self.status = status
 
 
-async def download_garmin_data(client, activity_id, folder, file_type="gpx"):
+def download_garmin_data(api, activity_id, folder):
     try:
-        file_data = await client.download_activity(activity_id, file_type=file_type)
-        file_path = os.path.join(folder, f"{activity_id}.{file_type}")
-        need_unzip = False
-        if file_type == "fit":
-            file_path = os.path.join(folder, f"{activity_id}.zip")
-            need_unzip = True
-        async with aiofiles.open(file_path, "wb") as fb:
-            await fb.write(file_data)
-        if need_unzip:
-            zip_file = zipfile.ZipFile(file_path, "r")
-            for file_info in zip_file.infolist():
-                zip_file.extract(file_info, folder)
-                os.rename(
-                    os.path.join(folder, f"{activity_id}_ACTIVITY.fit"),
-                    os.path.join(folder, f"{activity_id}.fit"),
-                )
-            os.remove(file_path)
+        file_data = api.download_activity(activity_id)
+        file_path = os.path.join(folder, f"{activity_id}.zip")
+
+        with open(file_path, "wb") as fb:
+            fb.write(file_data)
+
+        zip_file = zipfile.ZipFile(file_path, "r")
+        for file_info in zip_file.infolist():
+            zip_file.extract(file_info, folder)
+            p = os.path.join(folder, f"{activity_id}_ACTIVITY.fit")
+            if os.path.exists(p):
+                os.rename(p, os.path.join(folder, f"{activity_id}.fit"))
+            else:
+                gpx = os.path.join(folder, f"{activity_id}_ACTIVITY.gpx")
+                print(f'find gpx_id {activity_id}')
+                os.remove(gpx)
+        os.remove(file_path)
     except Exception as e:
         print(f"Failed to download activity {activity_id}: {str(e)}")
         traceback.print_exc()
-
-
-async def get_activity_id_list(client, start=0):
-    activities = await client.get_activities(start, 100)
-    if len(activities) > 0:
-        ids = list(map(lambda a: str(a.get("activityId", "")), activities))
-        print("Syncing Activity IDs")
-        return ids + await get_activity_id_list(client, start + 100)
-    else:
-        return []
 
 
 async def gather_with_concurrency(n, tasks):
@@ -196,29 +293,21 @@ def get_downloaded_ids(folder):
     return [
         i.split(".")[0]
         for i in os.listdir(folder)
-        if i.endswith(".gpx") and not i.startswith(".")
+        if i.endswith(".fit") and not i.startswith(".")
     ]
 
 
-async def download_new_activities(secret_string, auth_domain, downloaded_ids, is_only_running, folder, file_type):
-    client = Garmin(secret_string, auth_domain, is_only_running)
-    # because I don't find a para for after time, so I use garmin-id as filename
-    # to find new run to generage
-    activity_ids = await get_activity_id_list(client)
+def download_new_activities(secret_path, is_cn, downloaded_ids, folder):
+    api = Garmin(secret_path, is_cn)
+    activity_ids = api.get_activity_id_list()
+    print(f"all activity_ids size is {len(activity_ids)}")
     to_generate_garmin_ids = list(set(activity_ids) - set(downloaded_ids))
     print(f"{len(to_generate_garmin_ids)} new activities to be downloaded")
 
     start_time = time.time()
-    await gather_with_concurrency(
-        10,
-        [
-            download_garmin_data(client, id, folder, file_type=file_type)
-            for id in to_generate_garmin_ids
-        ],
-    )
+    for activity_id in to_generate_garmin_ids:
+        download_garmin_data(api, activity_id, folder)
     print(f"Download finished. Elapsed {time.time() - start_time} seconds")
-
-    await client.req.aclose()
     return to_generate_garmin_ids
 
 
@@ -233,28 +322,6 @@ if __name__ == "__main__":
         help="if garmin accout is cn",
     )
     parser.add_argument(
-        "--only-run",
-        dest="only_run",
-        action="store_true",
-        help="if is only for running",
-    )
-    parser.add_argument(
-        "--tcx",
-        dest="download_file_type",
-        action="store_const",
-        const="tcx",
-        default="gpx",
-        help="to download personal documents or ebook",
-    )
-    parser.add_argument(
-        "--fit",
-        dest="download_file_type",
-        action="store_const",
-        const="fit",
-        default="gpx",
-        help="to download personal documents or ebook",
-    )
-    parser.add_argument(
         "--out",
         dest="out_dir",
         action="store",
@@ -262,15 +329,10 @@ if __name__ == "__main__":
         help="download out file dir",
     )
     options = parser.parse_args()
-    secret_string = GarminLogin(options.user, options.pwd, options.is_cn).gen_secret()
-    auth_domain = (
-        "CN" if options.is_cn else "Global"
-    )
-    file_type = options.download_file_type
-    is_only_running = options.only_run
-    if secret_string == "":
-        print("username or password errror")
-        sys.exit(1)
+
+    p = '.garth_' + options.user
+    GarminLogin(options.user, options.pwd, options.is_cn).gen_secret(p)
+
     folder = options.out_dir
     # make gpx or tcx dir
     if not os.path.exists(folder):
@@ -279,17 +341,9 @@ if __name__ == "__main__":
 
     print(f'downloaded_ids size: {len(downloaded_ids)}')
 
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(
-        download_new_activities(
-            secret_string,
-            auth_domain,
-            downloaded_ids,
-            is_only_running,
-            folder,
-            file_type,
-        )
+    download_new_activities(
+        p,
+        options.is_cn,
+        downloaded_ids,
+        folder,
     )
-    loop.run_until_complete(future)
-
-    os.system(f"touch {folder}/success")
--- garmin_secret.py
+++ garmin_secret.py
@@ -15,18 +15,21 @@ class GarminLogin:
         if is_cn:
             self.domain = 'garmin.cn'
 
-    def gen_secret(self):
+    def gen_secret(self, p) -> bool:
         if self.domain:
             garth.configure(domain=self.domain)
         try:
-            p = '.garth_' + self.username
-            if os.path.exists(p):
-                return garth.client.load(p)
-            garth.login(self.username, self.password)
-            garth.client.dump(p)
+            if not os.path.exists(p):
+                print('new login')
+                garth.login(self.username, self.password)
+                garth.client.dump(p)
+            else:
+                print('load token from cache')
+            garth.client.load(p)
+            return True
         except Exception as e:
             print(e)
-            return ""
+            return False
 
 
 if __name__ == '__main__':
@@ -38,6 +41,7 @@ if __name__ == '__main__':
     options = parser.parse_args()
     print(options)
     login = GarminLogin(options.user, options.pwd, options.cn)
-    r = login.gen_secret()
-    if r != "":
+    p = '.garth_' + options.user
+    is_success = login.gen_secret(p)
+    if is_success:
         print('success')
